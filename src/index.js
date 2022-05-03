65b32ad79c862ef27a68404b7928f4647687e490
Douglas Martins <heredouglas@gmail.com>
2022-05-03 14:56:11 -0300

    segundo commit

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import Home from './pages/home'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
-  <BrowserRouter>
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
     <Routes>
         <Route path="/" element={<Home/>} />
         <Route path="/details/:id" element={<Details />} />
